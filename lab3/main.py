81be8b5318cd0a7a13c3d04a02af70734b1421e3
120L022221 <1264658252@qq.com>
2023-05-15 19:37:02 +0800

    test

--- lab3/main.py
+++ lab3/main.py
@@ -1,5 +1,6 @@
 import glob
 
+import pandas as pd
 import torch
 import torch.nn as nn
 import os
@@ -16,8 +17,9 @@ test_dir = "./test"
 train_dir = "./train"
 epochs = 100
 batch_size = 40
-learning_rate = 0.01
+learning_rate = 0.001
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 # 设置随机种子
 def setseed(seed):
@@ -25,7 +27,7 @@ def setseed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
 
 
 class GetTestLoader(torch.utils.data.Dataset):
@@ -51,7 +53,7 @@ img_size = 224
 # 翻转、旋转、移位等操作
 train_transforms = transforms.Compose([
     # transforms.RandomRotation(30),
-    transforms.Resize((img_size,img_size)),
+    transforms.Resize((img_size, img_size)),
     # transforms.RandomResizedCrop(img_size),
     # transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
@@ -65,7 +67,7 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
 
 # 加载测试数据
 test_transforms = transforms.Compose([
-    transforms.Resize((img_size,img_size)),
+    transforms.Resize((img_size, img_size)),
     # transforms.CenterCrop(img_size),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -100,6 +102,7 @@ class VGG11(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(inplace=True),
@@ -112,7 +115,7 @@ class VGG11(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        # x = self.avgpool(x)
+        x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
@@ -122,8 +125,9 @@ def train():
     model = VGG11()
     model = model.to(device)
     criterion = nn.CrossEntropyLoss().cuda()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[2, 5, 20, 40], gamma=0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[ 5, 20, 40], gamma=0.1)
     best_loss = 999999
     tb_writer = SummaryWriter(log_dir='VGG11_sgd_lr=0.0001_bs=32_epoch=70'
                                       '/logs', comment='VGG11')
@@ -141,7 +145,7 @@ def train():
             optimizer.step()
             train_loss += loss.item() * inputs.size(0)
             _, preds = torch.max(outputs, 1)
-        scheduler.step()
+        # scheduler.step()
         # 添加到tensorboard
         train_loss = train_loss / len(train_loader.dataset)
         tb_writer.add_scalar('Train Loss', train_loss, epoch)
@@ -153,6 +157,31 @@ def train():
     print(f'Best validation accuracy: {best_loss:.4f}')
 
 
+def test():
+    model = VGG11()
+    model.load_state_dict(torch.load('MODEL/' + 'VGG11_sgd_lr=0.01_bs=32_epoch=70' + '.pth'))
+    model = model.to(device)
+    model.eval()
+    num_labels = []
+    str_labels = []
+    classes = datasets.ImageFolder(train_dir).class_to_idx
+    dict_keys = list(classes.keys())
+    dict_values = list(classes.values())
+    with torch.no_grad():
+        for inputs in test_loader:
+            inputs = inputs.to(device)
+            outputs = model(inputs)
+            index, predicts = torch.max(outputs, 1)
+            num_labels = num_labels + predicts.detach().cpu().numpy().tolist()
+    for i in num_labels:
+        index = dict_values.index(i)
+        key = dict_keys[index]
+        str_labels.append(key)
+    file = os.listdir(test_dir)
+    submission_df = pd.DataFrame({'file': file, 'species': str_labels})
+    submission_df.to_csv('VGG11.csv', index=False)
+
 if __name__ == '__main__':
     setseed(123)
-    train()
+    # train()
+    test()
